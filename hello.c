6854a646de7923c10fcba6f47d60813952826ac1
Timur Gereihanov <62817154+t1pUr@users.noreply.github.com>
2021-12-15 22:19:00 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,67 +1,68 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/moduleparam.h>
 #include <linux/ktime.h>
-#include<linux/slab.h>
+#include <linux/slab.h>
 
-MODULE_AUTHOR(”Timur Gereihanov <simplex1135@gmail.com>”);
-MODULE_DESCRIPTION(”Lab5 AK”);
-MODULE_LICENSE(”Dual BSD/GPL”);
+MODULE_AUTHOR("Timur Gereihanov <simplex1135@gmail.com>");
+MODULE_DESCRIPTION("Lab5 AK");
+MODULE_LICENSE("Dual BSD/GPL");
 
+static uint howmany = 1;
 
- uint hello_world_count = 1;
- module_param(hello_world_count, uint, 0660);
- MODULE_PARM_DESC(hello_world_count, ”Number of ’hello world’s to print”);
+module_param(howmany, uint, 0444);
+MODULE_PARM_DESC(howmany, "Number of prints");
 
- struct hello_data {
- struct list_head tlist;
- ktime_t time;
- };
+struct my_struct {
+    struct list_head tlist;
+    ktime_t time;
+};
 
- LIST_HEAD(head_node);
+static LIST_HEAD(my_list_head);
 
- static int __init hello_init(void)
- {
- int i;
+static int __init hello_init(void)
+{
 
- printk(KERN_INFO ”Loading module hello...\n”);
+    if (howmany == 0) {
+		pr_warn("The parameter is 0");
+    } else if (howmany >= 5 && howmany <= 10) {
+		pr_warn("The parameter is in [5; 10]");
+    } else if (howmany > 10) {
+		pr_err("The parameter is > 10)");
+		return -EINVAL;
+    }
 
- if (hello_world_count == 0) {
- printk(KERN_WARNING ”Warning: hello_world_count is zero\n”);
- } else if (hello_world_count >= 5 && hello_world_count <= 10) {
- printk(KERN_WARNING ”Warning: hello_world_count is between 5 and 10\n”);
- } else if (hello_world_count > 10) {
- printk(KERN_ERR ”Error: hello_world_count is out of range\n”);
- return -EINVAL;
- }
+    int i;
+	for (i = 0; i < howmany; i++) {
 
- for (i = 0; i < hello_world_count; i++) {
- struct hello_data *new_node = NULL;
+		struct my_struct *ms = NULL;
 
- new_node = kmalloc(sizeof(struct hello_data), GFP_KERNEL);
- new_node->time = ktime_get();
- INIT_LIST_HEAD(&new_node->tlist);
- list_add_tail(&new_node->tlist, &head_node);
+		ms = kmalloc(sizeof(struct my_struct), GFP_KERNEL);
+		ms->time = ktime_get();
+		list_add_tail(&ms->tlist, &my_list_head);
 
- printk(KERN_INFO ”Hello, world!\n”);
- }
- return 0;
- }
+		pr_info("Hello, world!\n");
+    }
 
- static void __exit hello_exit(void)
- {
- int count = 0;
- struct hello_data *c, *temp_node;
+    return 0;
+}
 
- list_for_each_entry_safe(c, temp_node, &head_node, tlist) {
- printk(KERN_INFO ”Node %d ktime = %lld\n”, count++,
+static void __exit hello_exit(void)
+{
+    int count = 0;
+    struct my_struct *t, *tmp;
 
+    if (ZERO_OR_NULL_PTR(&my_list_head))
+    	return 0;
 
- ktime_to_ns(c->time));
- list_del(&c->tlist);
- kfree(c);
- }
- printk(KERN_INFO ”Module unloaded\n”);
- }
+    list_for_each_entry_safe(t, tmp, &my_list_head, tlist) {
+		pr_info("i: %d; ktime = %lld\n", count++, ktime_to_ns(t->time));
+		list_del(&t->tlist);
+		kfree(t);
+    }
+    pr_info("Module unloaded\n");
+}
 
- module_init(hello_init);
- module_exit(hello_exit);
+module_init(hello_init);
+module_exit(hello_exit);
